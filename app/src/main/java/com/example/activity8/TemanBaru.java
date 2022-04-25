aad98ef41c48ba6e836db06ebfe165bac771cb27
Istytsaniya <tsaniyaisty08@gmail.com>
2022-04-26 06:39:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity8/TemanBaru.java
@@ -0,0 +1,14 @@
+package com.example.activity8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TemanBaru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman_baru);
+    }
+}
